bb1b3e7c597c501259cb5a4145d842a90bafc02c
ruan <ruany@users.noreply.github.com>
2018-04-23 08:48:41 +0200

    Update jQuery to 3.3.1

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,3 @@
-<script src="<?php echo $this->autoversion('inc/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo $this->autoversion('inc/js/jquery-3.3.1.min.js'); ?>"></script>
 <script src="<?php echo $this->autoversion('inc/js/bootstrap.min.js'); ?>"></script>
 <?php echo "</html>"; ?>
