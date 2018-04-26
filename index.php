09ca583c3f11e1b68f665889ce2e00961029dfd4
ruan <ruany@users.noreply.github.com>
2018-04-26 07:41:06 +0200

    Refactor CSS

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ $page->print_title();
 <br>
 <div class="container">
     <div class="jumbotron">
-        <div class="litebans-index">
+        <div class="litebans-index litebans-index-main">
             <h2><?php echo str_replace("{server}", $page->settings->name, $page->t("index.welcome.main")); ?></h2>
         </div>
 
-        <div class="litebans-index"><p><?php echo $page->t("index.welcome.sub"); ?></p></div>
+        <div class="litebans-index litebans-index-sub"><p><?php echo $page->t("index.welcome.sub"); ?></p></div>
     </div>
 </div>
 <?php $page->print_footer(false); ?>
