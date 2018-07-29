301c10f0bec35333f6a9f13ba22b03a8e3773ba6
ruan <ruany@users.noreply.github.com>
2018-07-29 08:57:46 +0200

    Use php_ini_loaded_file() instead of phpinfo() hack

--- error/missing-extensions.php
+++ error/missing-extensions.php
@@ -16,26 +16,11 @@
             <br>
             <?php if (!extension_loaded("pdo_mysql")) {
                 echo "- <a class=\"text-danger\">pdo_mysql</a>";
-            } ?>
+            }
+            $phpini = php_ini_loaded_file();
 
-
-
-            <?php
-
-            ob_start();
-            phpinfo();
-            $phpinfo = array('phpinfo' => array());
-            if (preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER))
-                foreach ($matches as $match)
-                    if (strlen($match[1]))
-                        $phpinfo[$match[1]] = array();
-                    elseif (isset($match[3]))
-                        $phpinfo[end(array_keys($phpinfo))][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
-                    else
-                        $phpinfo[end(array_keys($phpinfo))][] = $match[2];
-
-            echo "These extensions can be enabled in php.ini.<br>";
-            echo "php.ini location: <a class=\"text-info\">" . $phpinfo['phpinfo']["Loaded Configuration File"] . "</a>";
+            echo "These extensions can be enabled in php.ini.<br><br>";
+            echo "php.ini location: <a class=\"text-info\">" . $phpini . "</a><br>";
             ?>
         </div>
         <br>
